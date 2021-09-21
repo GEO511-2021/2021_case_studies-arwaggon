38805737b9311943ba6ae9033712dd3d7c6705ce
Amanda Waggoner <arwaggon@buffalo.edu>
2021-09-21 10:11:36 -0400

    Week 1, Amanda Waggoner, Case Study

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,15 @@
-data(iris)
+#Beginning of script, load in Iris data
+data("iris")
+?mean
 
+#Calculate the mean of Petal.Length
+#Assign an object from the mean Petal.Length to be called petal_length_mean
+mean(iris$Petal.Length)
+petal_length_mean <- mean(iris$Petal.Length)
+
+#Create histogram
+hist(iris$Petal.Length)
+?hist
+?hist
+hist(iris$Petal.Length, main="Histogram of Petal Length", col="blue", border="black",
+     xlab="Petal Length data")
